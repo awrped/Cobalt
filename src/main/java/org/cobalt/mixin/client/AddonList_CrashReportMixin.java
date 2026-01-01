c60fd033a0ccccda0429b0e17af19a5a94a37661
Nathan <209938737+quiteboring@users.noreply.github.com>
2026-01-01 17:39:54 -0500

    feat: reimplement addonlist crashreport mixin by oblongboot

--- src/main/java/org/cobalt/mixin/client/AddonList_CrashReportMixin.java
+++ src/main/java/org/cobalt/mixin/client/AddonList_CrashReportMixin.java
@@ -1,17 +1,38 @@
 package org.cobalt.mixin.client;
 
 import net.minecraft.util.crash.CrashReport;
+import org.cobalt.internal.loader.AddonLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import java.util.stream.Collectors;
 
+/**
+ * Superb mixin by oblongboot!
+ */
 @Mixin(CrashReport.class)
 public abstract class AddonList_CrashReportMixin {
 
   @Inject(method = "addDetails", at = @At("HEAD"))
   private void addAddonInfo(StringBuilder crashReportBuilder, CallbackInfo ci) {
-    // TODO: Reimplement this when addons are a thing again
+    String addons = AddonLoader.INSTANCE.getAddons().stream()
+      .map(info -> info.getFirst().getName() + " v" + info.getFirst().getVersion())
+      .collect(Collectors.joining(", "));
+
+    if (addons.isEmpty()) {
+      addons = "None";
+    }
+
+    int count = AddonLoader.INSTANCE.getAddons().size();
+
+    crashReportBuilder
+      .append("\n========================================")
+      .append("\nCobalt Addons (")
+      .append(count)
+      .append("): ")
+      .append(addons)
+      .append("\n========================================\n");
   }
 
 }
